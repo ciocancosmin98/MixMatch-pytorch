922f7005789d08f307d27b43c044af796197470b
Ştefan Cosmin CIOCAN (94448) <stefan.ciocan0106@stud.acs.upb.ro>
2021-05-01 20:14:47 +0300

    Add tensorboard script compatible with sessions

---
+++ start_tensorboard.py
@@ -0,0 +1,52 @@
+from tensorboard import program
+import sys
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Custom Tensorboard launcher')
+
+parser.add_argument('--dataset-name', default='animals10', type=str, metavar='NAME',
+                    help='name of the dataset')
+parser.add_argument('--session-id', default=-1, type=int, metavar='ID',
+                    help='the id of the session to be visualized')
+
+args = parser.parse_args()
+
+if args.session_id == -1:
+    max_id = -1
+    sessions_dir = os.path.join('sessions', args.dataset_name)
+    for fname in os.listdir(sessions_dir):
+        try:
+            _id = int(fname)
+
+            if _id >= max_id:
+                max_id = _id
+        except ValueError:
+            pass
+
+    if max_id == -1:
+        raise Exception("Could not find any sessions using the dataset " + \
+            args.dataset_name + ".")
+    _id = max_id
+else:
+    session_dir = os.path.join('sessions', args.dataset_name, str(args.session_id))
+    if not os.path.exists(session_dir):
+        raise Exception("Could not find session " + str(args.session_id) + \
+            " using the dataset " + args.dataset_name + ".")
+    _id = args.session_id
+
+tb = program.TensorBoard()
+logdir = os.path.join('sessions', args.dataset_name, str(_id), 'tensorboard')
+tb.configure(argv=[None, '--logdir', logdir])
+url = tb.launch()
+
+print('Open in browser:', url)
+
+exit_list = ['exit', 'quit', 'q']
+
+print('Type one of', exit_list, 'to quit')
+
+for line in sys.stdin:
+    if line.rstrip() in exit_list:
+        break
+    
