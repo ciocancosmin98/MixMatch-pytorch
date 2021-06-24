832f096a6ee46104bddb7531cfebfb8246868fe2
Cosmin Ciocan <ciocan.cosmin98@gmail.com>
2021-06-24 21:30:03 +0300

    Add new predict and draw function

--- predict.py
+++ predict.py
@@ -20,7 +20,7 @@ def top_k(values, k=3):
     return indexed[:k]
 
 def get_wideresnet_models(n_classes):
-    print("==> creating WRN-28-2")
+    #print("==> creating WRN-28-2")
 
     def create_model(ema=False):
         model = models.WideResNet(num_classes=n_classes)
@@ -35,7 +35,7 @@ def get_wideresnet_models(n_classes):
     model = create_model()
     ema_model = create_model(ema=True)
 
-    print('    Total params: %.2fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
+    #print('    Total params: %.2fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
     return model, ema_model
 
 def get_image_filenames(session_dir):
@@ -178,11 +178,146 @@ def show_prediction_grid(session_dir, model, is_best):
     if is_best:
         cv2.imwrite(os.path.join(images_dir, 'grid_best.jpg'), img)
 
+
+def create_grid(images, image_size = 64, grid_width = 5, grid_height = 2):
+
+    #TEXT_LEN = int(SIZE // 2 * 3)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    scale = 0.4 * image_size / 256
+    thickness = 1
+
+    sess_dir_ssl = 'sessions\\oregon_wildlife\\6'
+    sess_dir_supervised = 'sessions\\oregon_wildlife\\4'
+
+    result = np.full((image_size * grid_height, image_size * grid_width, 3), fill_value=1.0)
+
+    for index, (fname, _class) in enumerate(images):
+        #result_ssl = predict(fname, sess_dir_ssl)
+        #result_sup = predict(fname, sess_dir_supervised)
+
+        index = min(grid_width * grid_height - 1, index)
+
+        grid_x = index  % grid_width
+        grid_y = index // grid_width
+
+        start_x = grid_x * image_size
+        start_y = grid_y * image_size
+
+        img = cv2.imread(fname)
+        img = resize(img, size=image_size)
+
+        theight = int(image_size * 0.23)
+
+        img[image_size - theight:, :, :] = img[image_size - theight:, :, :] / 2
+        img[:theight, :, :] = img[:theight, :, :] / 2
+
+        text_starty = int((image_size - theight) + scale * 35)
+        result_ssl = predict(fname, sess_dir_ssl)
+        for id, (_class_name, percent) in enumerate(result_ssl):
+            color = (100, 255, 100) if _class_name == _class else (255, 255, 255)
+            text = _class_name + ':'
+            start = (4, text_starty + id * theight // 3)
+            img = cv2.putText(img, text, start, font, scale, color, thickness, cv2.LINE_AA)
+
+            text = str(percent)
+            start = (int(image_size - scale * 150), text_starty + id * theight // 3)
+            img = cv2.putText(img, text, start, font, scale, color, thickness, cv2.LINE_AA)
+
+
+        text_starty = int(scale * 35)
+        result_sup = predict(fname, sess_dir_supervised)
+        for id, (_class_name, percent) in enumerate(result_sup):
+            color = (100, 255, 100) if _class_name == _class else (255, 255, 255)
+            text = _class_name + ':'
+            start = (4, text_starty + id * theight // 3)
+            img = cv2.putText(img, text, start, font, scale, color, thickness, cv2.LINE_AA)
+
+            text = str(percent)
+            start = (int(image_size - scale * 150), text_starty + id * theight // 3)
+            img = cv2.putText(img, text, start, font, scale, color, thickness, cv2.LINE_AA)
+
+        """
+        start = (4, text_starty)
+        img = cv2.putText(img, _class, start, font, scale, color, thickness, cv2.LINE_AA)
+
+        start = (4, text_starty + theight // 3)
+        img = cv2.putText(img, _class, start, font, scale, color, thickness, cv2.LINE_AA)
+
+        start = (4, text_starty + 2 * theight // 3)
+        img = cv2.putText(img, _class, start, font, scale, color, thickness, cv2.LINE_AA)
+
+        start = (4 + image_size // 2, text_starty)
+        img = cv2.putText(img, _class, start, font, scale, color, thickness, cv2.LINE_AA)
+
+        start = (4 + image_size // 2, text_starty + theight // 3)
+        img = cv2.putText(img, _class, start, font, scale, color, thickness, cv2.LINE_AA)
+
+        start = (4 + image_size // 2, text_starty + 2 * theight // 3)
+        img = cv2.putText(img, _class, start, font, scale, color, thickness, cv2.LINE_AA)
+        """
+
+        result[start_y : start_y + image_size, start_x : start_x + image_size, :] = img
+
+    
+    return result
+
+#img_path = 'data\\oregon_wildlife\\new_images\\deer_01.jpg'
+#img_path = 'data\\oregon_wildlife\\new_images\\bald_eagle_01.jpg'
+#img_path = 'data\\oregon_wildlife\\new_images\\black_bear_01.jpg'
+#img_path = 'data\\oregon_wildlife\\new_images\\bobcat_01.jpg'
+#img_path = 'data\\oregon_wildlife\\new_images\\canadian_lynx.jpg'
+#img_path = 'data\\oregon_wildlife\\new_images\\cougar_01.jpg'
+#img_path = 'data\\oregon_wildlife\\new_images\\coyote_01.jpg'
+#img_path = 'data\\oregon_wildlife\\new_images\\elk_01.jpg'
+#img_path = 'data\\oregon_wildlife\\new_images\\gray_fox_01.png'
+#img_path = 'data\\oregon_wildlife\\new_images\\sea_lion_01.jpg'
+
+#results = {}
+
+#for fname in os.listdir('data\\oregon_wildlife\\new_images'):
+#    fpath = os.path.join('data\\oregon_wildlife\\new_images', fname)
+
+#    result_ssl = predict(fpath, sess_dir_ssl)
+#    result_sup = predict(fpath, sess_dir_supervised)
+
+#    results[fname.split('.')[0]] = {'ssl' : result_ssl, 'sup' : result_sup}
+
+#for animal in results:
+#    print(animal + ':')
+
+#    rslt = results[animal]
+#    for model in rslt:
+#        print(' ' * 4 + model + ':')
+
+#        mdl = rslt[model]
+#        for _class, perc in mdl:
+#            print(' ' * 8 + (_class + ':').ljust(16) + str(perc))
+
 """
-sess_dir = 'sessions\\oregon_wildlife\\1'
-img_path = 'data\\oregon_wildlife\\images\\nutria\\1cb31f27c4685c6e0d.jpg'
-img = cv2.imread(img_path)
-result = predict(img_path, sess_dir)
-print(result)
+images = [
+    ('data\\oregon_wildlife\\new_images\\deer_01.jpg', 'deer'),
+    ('data\\oregon_wildlife\\new_images\\bald_eagle_01.jpg', 'bald_eagle'),
+    ('data\\oregon_wildlife\\new_images\\black_bear_01.jpg', 'black_bear'),
+    ('data\\oregon_wildlife\\new_images\\bobcat_01.jpg', 'bobcat'),
+    ('data\\oregon_wildlife\\new_images\\canadian_lynx.jpg', 'canada_lynx'),
+    ('data\\oregon_wildlife\\new_images\\cougar_01.jpg', 'cougar'),
+    ('data\\oregon_wildlife\\new_images\\coyote_01.jpg', 'coyote'),
+    ('data\\oregon_wildlife\\new_images\\elk_01.jpg', 'elk'),
+    ('data\\oregon_wildlife\\new_images\\gray_fox_01.png', 'gray_fox'),
+]
+
+#('data\\oregon_wildlife\\new_images\\sea_lion_01.jpg', 'sea_lions')
+
+img = create_grid(images, 256, 3, 3)
+cv2.imwrite('grid_3x3.jpg', img)
+"""
+
+
+
+
+#img = cv2.imread(img_path)
+#result = predict(img_path, sess_dir_ssl)
+#print(result)
+#result = predict(img_path, sess_dir_supervised)
+#print(result)
 #predict2(img, sess_dir)
-"""
